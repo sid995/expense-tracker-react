8cb00f30c2241a982bec51847c0e87a3eb5de5e4
Siddharth Kundu <sk31395@gmail.com>
2020-02-25 22:52:24 +0530

    Complete version

---
+++ src/Components/Transaction.js
@@ -0,0 +1,21 @@
+import React, { useContext } from 'react'
+
+import { GlobalContext } from '../Context/GlobalState'
+
+export const Transaction = ({ transaction }) => {
+	const { deleteTransaction } = useContext(GlobalContext)
+	const sign = transaction.amount < 0 ? '-' : '+'
+	const className = transaction.amount < 0 ? 'minus' : 'plus'
+
+	return (
+		<li className={className}>
+			{transaction.text}{' '}
+			<span>
+				{sign} ${Math.abs(transaction.amount)}
+			</span>
+			<button className='delete-btn' onClick={() => deleteTransaction(transaction.id)}>
+				x
+			</button>
+		</li>
+	)
+}
---
+++ src/Components/TransactionList.js
@@ -0,0 +1,17 @@
+import React, { useContext } from 'react'
+import { GlobalContext } from '../Context/GlobalState'
+import { Transaction } from '../Components/Transaction'
+
+export const TransactionList = () => {
+	const { transactions } = useContext(GlobalContext)
+	return (
+		<>
+			<h3>History</h3>
+			<ul className='list'>
+				{transactions.map(transaction => (
+					<Transaction key={transaction.id} transaction={transaction} />
+				))}
+			</ul>
+		</>
+	)
+}
